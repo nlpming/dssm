9da63898dc0349f116a7e809c0d7a8a4939df308
nlpming <qqchenzhiming@163.com>
2022-01-03 11:41:28 +0800

    update ../triplet/convert_data.py

--- triplet/convert_data.py
+++ triplet/convert_data.py
@@ -29,13 +29,17 @@ def convert_tfrecord(in_file, out_file, vocab_path, query_size=50, doc_size=200)
     with codecs.open(in_file, "r", "utf-8") as fr:
         for line in tqdm(fr):
             icount += 1
-            line = line.strip().split("\t")
-            query = sent2id(line[0], vocab, query_size)
-            doc = sent2id(line[1], vocab, doc_size)
+            data = json.loads(line)
+            # 输入数据：query,doc_pos,doc_neg
+            query = sent2id(data["query"], vocab, query_size)
+            doc_pos = sent2id(data["doc_pos"], vocab, doc_size)
+            doc_neg = sent2id(data["doc_neg"], vocab, doc_size)
 
-            feed_dict = {"query_char": create_int_feature(query),
-                         "doc_char": create_int_feature(doc),
-                         "label": create_int_feature([1])}
+            feed_dict = {
+                "query": create_int_feature(query),
+                "doc_pos": create_int_feature(doc_pos),
+                "doc_neg": create_int_feature(doc_neg)
+            }
             example = tf.train.Example(features=tf.train.Features(feature=feed_dict))
             serialized = example.SerializeToString()
             writer.write(serialized)
@@ -45,8 +49,8 @@ def convert_tfrecord(in_file, out_file, vocab_path, query_size=50, doc_size=200)
 
 
 if __name__ == "__main__":
-    convert_tfrecord("./data/data.txt", 
-                     "./data/train.tfrecord", 
-                     "./char.json", 
+    convert_tfrecord("../data/data_triplet.json",
+                     "../data/train_triplet.tfrecord",
+                     "../char.json",
                      query_size=50, 
                      doc_size=200)
